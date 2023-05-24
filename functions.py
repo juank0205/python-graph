f1e9998880453eea15c4ff91b36591396971eb8e
juank0205 <jkamiloeche@outlook.com>
2023-05-23 19:56:46 -0500

    first graphics

--- functions.py
+++ functions.py
@@ -1,22 +1,37 @@
 import numpy as np
+import math
 class Function(object):
-    def __init__(self, string:str, independent:str, dependent:str) -> None:
+    def __init__(self, string:str) -> None:
         self.string = string
-        self.independent = independent
-        self.dependent = dependent
         self.__isDigit = self.string.isdigit()
 
     #Getters
-    def get_isDigit(self):
+    def get_isDigit(self) -> bool:
         return(self.__isDigit)
 
-    def evalFunction(self, xArray):
+    def evalFunction(self, xArray) -> tuple:
         try:
-            yArray = eval(self.string, {'x': xArray, 'np': np} ) if not(self.__isDigit) else [float(self.string)]*len(xArray)
+            yArray = eval(self.string, {'x': xArray, 'np': np, 'math': math} ) if not(self.__isDigit) else [float(self.string)]*len(xArray)
             return (xArray, yArray)
         except:
-            return 0
+            return (0, 0)
         
 
-mifuncion = Function("3", "x", "y")
+class differentialEquation(Function):
+    def __init__(self, string: str, initialPoint: tuple) -> None:
+        super().__init__(string) 
+        self.initialPoint = initialPoint
+
+    def __evalFunction(self, xArray, yArray = 0) -> tuple:
+        try:
+            yArray = eval(self.string, {'t': xArray, 'y': yArray, 'np': np, 'math': math} ) if not(self.__isDigit) else [float(self.string)]*len(xArray)
+            return (xArray, yArray)
+        except:
+            return (0, 0)
+        
+    def eulerMethod(self) -> None:
+        pass
+
+        
+mifuncion = Function("3*x-2")
 print(mifuncion.evalFunction(np.arange(-10, 10, 1)))
