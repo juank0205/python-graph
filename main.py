159044c1b0cf33de778ec335d9901b3a58a5c653
juank0205 <jkamiloeche@outlook.com>
2023-05-25 12:06:26 -0500

    sidebar

--- main.py
+++ main.py
@@ -5,36 +5,15 @@ from widgets import SlidePanel
 window = ctk.CTk()
 window.title('Grapher')
 window.geometry('600x400')
+ctk.set_appearance_mode('dark')
 
 #Animated widget
-animated_panel = SlidePanel(window, 0, 0.3)
-
-#Move the button when the menu is toggled
-def move_btn():
-    global button_x
-    global isToggled
-    if isToggled:
-        if button_x >= 0.15:
-            button_x -= 0.01
-            window.after(50, move_btn)
-        else:
-            isToggled = False
-            button.place(relx = button_x, rely = 0.1, anchor = 'center')
-            return
-    else:
-        if button_x <= 0.3:
-            button_x += 0.01
-            window.after(50, move_btn)
-        else:
-            isToggled = True
-            button.place(relx = button_x, rely = 0.1, anchor = 'center')
-            return
-    button.place(relx = button_x, rely = 0.1, anchor = 'center')
+animated_panel = SlidePanel(window, -0.3, 0)
 
 #Menu button
 isToggled = False
 button_x = 0.15
-button = ctk.CTkButton(window, text='Menu', command = move_btn)
+button = ctk.CTkButton(window, text='Menu', command = animated_panel.animate)
 button.place(relx = button_x, rely = 0.1, anchor = 'center')
 
 #Main loop
--- widgets.py
+++ widgets.py
@@ -1,13 +1,58 @@
 import customtkinter as ctk
 
-class SlidePanel(ctk.CTkFrame):
+class AnimatedWidget(object):
+    def __init__(self):
+        self.is_running = False
+        self.is_toggled = False
+
+    def animate(self):
+        if self.is_running:
+            return
+        self.is_running = True
+        if self.is_toggled:
+            self.animate_backwards()
+        else:
+            self.animate_forward()
+        self.is_toggled = not self.is_toggled
+
+    def animate_forward(self):
+        pass
+
+    def animate_backwards(self):
+        pass
+
+class SlidePanel(ctk.CTkFrame, AnimatedWidget):
     def __init__(self, parent, start_pos, end_pos):
         super().__init__(master = parent)
+        AnimatedWidget.__init__(self)
 
         #Properties
         self.start_pos = start_pos
         self.end_pos = end_pos
         self.width = abs(start_pos-end_pos)
 
+        #Animation status
+        self.pos = self.start_pos
+        self.__speed = 0.008
+        self.__delay = 10
+
         #Layout
-        self.place(relx = self.start_pos, rely = 0, relwidth = self.width, relheight = 1)
+        self.place(relx = self.start_pos, rely = 0.05, relwidth = self.width, relheight = 0.9)
+
+    def animate_forward(self):
+        if self.pos < self.end_pos:
+            self.pos += self.__speed
+            self.place(relx = self.pos, rely = 0.05, relwidth = self.width, relheight = 0.9)
+            self.after(self.__delay, self.animate_forward)
+        else:
+            self.is_running = False
+
+    def animate_backwards(self):
+        if self.pos > self.start_pos:
+            self.pos -= self.__speed
+            self.place(relx = self.pos, rely = 0.05, relwidth = self.width, relheight = 0.9)
+            self.after(self.__delay, self.animate_backwards)
+        else:
+            self.is_running = False
+
+
