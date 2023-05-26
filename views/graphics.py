2fab3234a3b080f77ff3056742bc47af843102ce
juank0205 <jkamiloeche@outlook.com>
2023-05-26 18:20:13 -0500

    View classes implementation

---
+++ views/graphics.py
@@ -0,0 +1,7 @@
+from views.views import View
+
+class GraphicView(View):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        #Content to be defined
---
+++ views/views.py
@@ -0,0 +1,27 @@
+import customtkinter as ctk
+
+class View(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(master=parent)
+    
+    def raise_view(self):
+        self.tkraise()
+
+class ViewContainer(ctk.CTkFrame):
+    def __init__(self, parent, view_dict={}):
+        super().__init__(master=parent)
+        self.__active_view = ""
+        self.view_dict = view_dict
+
+    #Setters
+    def get_active_view(self):
+        return self.__active_view
+
+    #Getters
+    def set_view_dict(self, view_dict):
+        self.view_dict = view_dict
+    
+    def change_view(self, name):
+        if self.__active_view is not name:
+            self.view_dict[name].tkraise()
+            self.__active_view = name
