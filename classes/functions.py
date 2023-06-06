9901ca8862ed631c82c3460da57e13ed9859b16a
juank0205 <jkamiloeche@outlook.com>
2023-06-05 23:47:26 -0500

    euler method

--- classes/functions.py
+++ classes/functions.py
@@ -5,6 +5,10 @@ class Function(object):
         self.string = string
         self.__isDigit = self.string.isdigit()
 
+    def set_string(self, string):
+        self.string = string
+        self.__isDigit = self.string.isdigit()
+
     #Getters
     def get_isDigit(self) -> bool:
         return(self.__isDigit)
@@ -21,16 +25,52 @@ class differentialEquation(Function):
     def __init__(self, string: str, initialPoint: tuple) -> None:
         super().__init__(string) 
         self.initialPoint = initialPoint
+        self.__step = 0.1 
+        self.__isDigit = True
 
-    def __evalFunction(self, xArray, yArray = 0) -> tuple:
+    def set_string(self, string):
+        self.string = string
+
+    def set_initialPoint(self, x, y):
+        self.initialPoint = (x, y)
+
+    def __evalFunction(self, xArray, yArray) -> tuple:
         try:
-            yArray = eval(self.string, {'t': xArray, 'y': yArray, 'np': np, 'math': math} ) if not(self.__isDigit) else [float(self.string)]*len(xArray)
+            # yArray = eval(self.string, {'t': xArray, 'y': yArray, 'np': np, 'math': math} ) if not(self.__isDigit) else [float(self.string)]*len(xArray)
+            yArray = eval(self.string, {'t': xArray, 'y': yArray, 'np': np, 'math': math})
             return (xArray, yArray)
         except:
-            return (0, 0)
+            print("Not valid")
+            return(0, 0)
+
+    def __plotStep(self, x, y, xi, yi):
+        return([x, xi], [y, yi])
         
-    def eulerMethod(self) -> None:
-        pass
+    def eulerMethod(self, plot_callback):
+        xRange = np.arange(self.initialPoint[0], 10, self.__step)
+        yi = self.initialPoint[1]
+        try:
+            for i in range(len(xRange)-1):
+                m = self.__evalFunction(xRange[i], yi)[1]
+                data = self.__plotStep(xRange[i], yi, xRange[i+1], yi+(m*self.__step))
+                plot_callback(data[0], data[1], color='#FF0000')
+                yi+=m*self.__step
+        except:
+            print("Invalid function")
+            return
+
+        xRange = np.arange(self.initialPoint[0], -10, self.__step*(-1))
+        yi = self.initialPoint[1]
+        try:
+            for i in range(len(xRange)-1):
+                m = self.__evalFunction(xRange[i], yi)[1]
+                data = self.__plotStep(xRange[i], yi, xRange[i+1], yi-(m*self.__step))
+                plot_callback(data[0], data[1], color='#FF0000')
+                yi-=m*self.__step
+        except:
+            print("Invalid function")
+            return
+
 
 def createFunction(string: str) ->Function:
     return Function(string)
--- views/graphics.py
+++ views/graphics.py
@@ -1,21 +1,145 @@
 from views.views import View
 import customtkinter as ctk
+from classes.functions import Function, differentialEquation
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+# Implement the default Matplotlib key bindings.
+from matplotlib.backend_bases import key_press_handler
+import numpy as np
 
-        #Background color = 242424
+
 class GraphicView(View):
     def __init__(self, parent):
         super().__init__(parent )
         self.place(relwidth=1, relheight=1)
+        self.x_min = -10
+        self.x_max = 10
+        self.y_min = -10
+        self.y_max = 10
+        self.__function = Function("")
+        self.__string = ctk.StringVar(parent, value='')
 
         #Content to be defined
-        Title = ctk.CTkLabel(master=self, text="Graphics view")
-        Title.place(relx=0.5, rely=0.5)
+        self.graph_frame = ctk.CTkFrame(master=self)
+        self.function_container = ctk.CTkFrame(master = self, width=300, height=50)
+        self.function_input = ctk.CTkEntry(master = self.function_container, width=220, height=30)
+        self.function_input.place(y=10, x=10)
+        self.function_button = ctk.CTkButton(master = self.function_container, width=60, height=30)
+        self.function_button.place(y=10, x=10)
 
-class EulerView(View):
+        self.fig, self.ax = plt.subplots()
+        self.fig.set_size_inches(20, 5)
+        self.fig.patch.set_facecolor('#ffffff')
+
+        self.set_plot_style()
+
+        self.canvas = FigureCanvasTkAgg(self.fig,master=self.graph_frame)
+        self.canvas.draw()
+
+        toolbar = NavigationToolbar2Tk(self.canvas, self.graph_frame)
+        toolbar.update()
+
+        self.canvas.mpl_connect(
+            "key_press_event", lambda event: print(f"you pressed {event.key}"))
+        self.canvas.mpl_connect("key_press_event", key_press_handler)
+
+        self.graph_frame.pack()
+        self.canvas.get_tk_widget().pack()
+        toolbar.pack()
+
+        self.function_entry = ctk.CTkEntry(master=self, textvariable=self.__string)
+        self.function_entry.pack(pady=10)
+
+        ctk.CTkButton(master=self, text='Graph', command=self.change_graph).pack(pady=10)
+
+    def set_plot_style(self):
+
+        #set self.axes limits and aspect ratio
+        self.ax.set(xlim=(self.x_min-1, self.x_max+1), ylim=(self.y_min-1, self.y_max+1), aspect='equal')
+
+        #set self.axes position and visibility to look like a cartesian plane
+        self.ax.spines['left'].set_position('zero')
+        self.ax.spines['bottom'].set_position('zero')
+        self.ax.spines['right'].set_color('none')
+        self.ax.spines['top'].set_color('none')
+
+        #set self.axes ticks style
+        self.ax.set_xlabel('$t$', size=14, x=1.02, labelpad=-24)
+        self.ax.set_ylabel('$y$', size=14, y=1, labelpad=-30, rotation=0)
+
+        #set self.axes ticks
+        self.ax.xaxis.set_major_locator(ticker.MaxNLocator(11))
+        self.ax.yaxis.set_major_locator(ticker.MaxNLocator(11))
+        yticks = self.ax.get_yticks()
+        self.ax.set_yticks([tick for tick in yticks if tick != 0])
+        self.ax.tick_params(axis='x', labelrotation=-45)
+
+        #Create a grid (decoration)
+        self.ax.grid(which='both', color='#a5a5a5', linestyle='-', linewidth=1, alpha=0.5)
+        
+
+    def change_graph(self):
+        self.__function.set_string(self.__string.get())
+        x_data, y_data = self.__function.evalFunction(np.arange(-10, 10, 0.1))
+        self.ax.cla()
+        self.set_plot_style()
+        self.ax.plot(x_data, y_data)
+        self.canvas.draw()
+        
+
+class EulerView(GraphicView):
     def __init__(self, parent):
-        super().__init__(parent)
+        View.__init__(self, parent)
         self.place(relwidth=1, relheight=1)
+        self.x_min = -10
+        self.x_max = 10
+        self.y_min = -10
+        self.y_max = 10
+        self.initialPoint = (0, 0)
+        self.__function = differentialEquation("", self.initialPoint)
+        self.__string = ctk.StringVar(parent, value='')
 
         #Content to be defined
-        Title = ctk.CTkLabel(master=self, text="Euler view")
-        Title.place(relx=0.5, rely=0.5)
+        self.graph_frame = ctk.CTkFrame(master=self)
+        self.function_container = ctk.CTkFrame(master = self, width=300, height=50)
+        self.function_input = ctk.CTkEntry(master = self.function_container, width=220, height=30)
+        self.function_input.place(y=10, x=10)
+        self.function_button = ctk.CTkButton(master = self.function_container, width=60, height=30)
+        self.function_button.place(y=10, x=10)
+
+        self.fig, self.ax = plt.subplots()
+        self.fig.set_size_inches(20, 5)
+        self.fig.patch.set_facecolor('#ffffff')
+
+        self.set_plot_style()
+
+        self.canvas = FigureCanvasTkAgg(self.fig,master=self.graph_frame)
+        self.canvas.draw()
+
+        toolbar = NavigationToolbar2Tk(self.canvas, self.graph_frame)
+        toolbar.update()
+
+        self.canvas.mpl_connect(
+            "key_press_event", lambda event: print(f"you pressed {event.key}"))
+        self.canvas.mpl_connect("key_press_event", key_press_handler)
+
+        self.graph_frame.pack()
+        self.canvas.get_tk_widget().pack()
+        toolbar.pack()
+
+        self.function_entry = ctk.CTkEntry(master=self, textvariable=self.__string)
+        self.function_entry.pack(pady=10)
+
+        ctk.CTkButton(master=self, text='Graph', command=self.change_graph).pack(pady=10)
+
+    def change_graph(self):
+        print(self.__string.get())
+        self.__function.set_string(self.__string.get())
+        self.ax.cla()
+        self.set_plot_style()
+        self.__function.eulerMethod(self.ax.plot)
+        self.canvas.draw()
+
